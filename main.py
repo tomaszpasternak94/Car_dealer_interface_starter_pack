c0c87e2240453005a9fa84caac1e31557697d8d5
Tomek <t.pasternak94@gmail.com>
2020-10-17 20:38:02 +0200

    feat(10) : repurchase car method with custom exception added to parking class

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ class Parking:
         self.cars_amount = 0
         self.car_dealer_cash = 100000
         self.cars_list = []
+
+    def repurchase_car(self, car):
+        """ This method allows the car dealer to repurchase a used car """
+        if self.car_dealer_cash > car.price:
+            self.cars_amount += 1
+            self.car_dealer_cash -= car.price
+            self.cars_list.append(car.brand)
+        else:
+            raise TooExpensive("Not enough money to repurchase this car")
