c83c3594c51b79aeebfe2acc94e99fe3495a5702
Tomek <t.pasternak94@gmail.com>
2020-10-17 20:34:55 +0200

    feat(9) : added main.py file with basic parking class definition

---
+++ main.py
@@ -0,0 +1,6 @@
+class Parking:
+    """ Car dealer interface starter pack """
+    def __init__(self):
+        self.cars_amount = 0
+        self.car_dealer_cash = 100000
+        self.cars_list = []
