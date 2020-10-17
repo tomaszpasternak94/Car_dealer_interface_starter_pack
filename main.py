f28a1d36eb943a33f587f52e4e381ea3bd8ab48d
Tomek <t.pasternak94@gmail.com>
2020-10-17 20:39:34 +0200

    feat(11) : sell car method with custom exception added to parking class

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ class Parking:
             self.cars_list.append(car.brand)
         else:
             raise TooExpensive("Not enough money to repurchase this car")
+
+    def sell_car(self, car):
+        """ This method allows the car dealer to sell a car from his parking lot """
+        if self.cars_amount:
+            self.car_dealer_cash += car.price + (car.price * 0.2)
+            self.cars_amount -= 1
+            self.cars_list.remove(car.brand)
+        else:
+            raise EmptyParking("Empty parking lot - all cars sold")
+
